25760fb3cadb647d78d8e3322d97df296e868a52
Richard Ram <r.ram@ma-web.nl>
2021-11-15 13:29:24 +0100

    implemented a quick and dirty gameloop

---
+++ InventorySystem/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+
+namespace InventorySystem
+{
+    public class Game
+    {
+        private Inventory _inventory;
+        private bool _shouldRun;
+        
+        public void Run()
+        {
+            _shouldRun = true;
+            _inventory = SetupInventory();
+
+            while (_shouldRun)
+            {
+                CheckForInput();
+                Update();
+            }
+
+            Shutdown();
+        }
+
+        private void CheckForInput()
+        {
+            Console.Write("Input for this frame: ");
+            string input = Console.ReadLine();
+            if (input == "")
+                return;
+
+            input = input.ToLower();
+            
+            switch (input)
+            {
+                case "exit":
+                    _shouldRun = false;
+                    break;
+                
+                case "1":
+                    _inventory.AddItem(new Weapon("Awesome Axe", EquipSlot.WeaponType.TwoHand, new WeaponStats(10, 5)));
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+
+        private void Update()
+        {
+            UsableItem[] items = _inventory.GetAllItemsOfType<UsableItem>();
+            Console.WriteLine(items.Length);
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+                item.OnUse();
+            }
+            
+            Weapon[] weapons = _inventory.GetAllItemsOfType<Weapon>();
+            foreach (Weapon weapon in weapons)
+            {
+                weapon.Attack();
+            }
+        }
+
+        private Inventory SetupInventory()
+        {
+            Inventory inventory = new Inventory();
+            
+            inventory.AddItem(new HealthPotion("HealthPotion", 10));
+            inventory.AddItem(new ManaPotion("ManaPotion", 5));
+            inventory.AddItem(new Weapon("BroadSword", EquipSlot.WeaponType.MainHand, new WeaponStats(5, 2)));
+
+            return inventory;
+        }
+        
+        private void Shutdown()
+        {
+            Console.WriteLine("Shutdown Application");
+        }
+
+    }
+}
--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -7,25 +7,8 @@ namespace InventorySystem
     {
         static void Main(string[] args)
         {
-            Inventory inventory = new Inventory();
-            
-            inventory.AddItem(new HealthPotion("HealthPotion", 10));
-            inventory.AddItem(new ManaPotion("ManaPotion", 5));
-            inventory.AddItem(new Weapon("BroadSword", EquipSlot.WeaponType.MainHand, new WeaponStats(5, 2)));
-
-            UsableItem[] items = inventory.GetAllItemsOfType<UsableItem>();
-            Console.WriteLine(items.Length);
-            foreach (var item in items)
-            {
-                Console.WriteLine(item.Name);
-                item.OnUse();
-            }
-
-            Weapon[] weapons = inventory.GetAllItemsOfType<Weapon>();
-            foreach (Weapon weapon in weapons)
-            {
-                weapon.Attack();
-            }
+            Game game = new Game();
+            game.Run();
         }
     }
 }
